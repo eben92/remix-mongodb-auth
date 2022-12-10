8a4072d20a8f522abce8bfd7341a55b1ec739f6b
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2022-12-10 15:47:02 +0000

    added  daily cron job

--- app/cron/index.ts
+++ app/cron/index.ts
@@ -1,11 +1,13 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { writeFileSync, readFileSync } from 'fs';
 import path from 'path';
-const file = path.join(process.cwd(), 'files', 'cron.json');
+const file = path.join(process.cwd(), 'cron.json');
 
 export async function getStoredCrons() {
   // const rawFileContent = await fs.readFile('cron.json', { encoding: 'utf-8' });
-  const stringified = readFileSync(file, 'utf-8');
-  const data = JSON.parse(stringified);
+  // const rawFileContent = await fs.readFile('cron.json', { encoding: 'utf-8' });
+  // const data = JSON.parse(rawFileContent);
+  const rawFileContent = readFileSync(file, { encoding: 'utf-8' });
+  const data = JSON.parse(rawFileContent);
 
   const storedCrons = data.jobs ?? [];
   return storedCrons;
