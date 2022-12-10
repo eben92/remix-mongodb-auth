7973580c2978ad4620ed9a15806961876693668e
rhutmann <pappy.rhutmannjnrolege@gmail.com>
2022-12-10 02:01:05 +0000

    added signin method

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,6 +1,8 @@
 import { json } from '@remix-run/node';
 import { prisma } from './prisma.server';
-import { RegisterForm } from './types.server';
+import type { RegisterForm, LoginForm } from './types.server';
+import { createUser } from './user.server';
+import bcrypt from 'bcrypt';
 
 export const register = async (form: RegisterForm) => {
   const exists = await prisma.user.count({
@@ -17,4 +19,40 @@ export const register = async (form: RegisterForm) => {
       }
     );
   }
+
+  const newUser = await createUser(form);
+
+  if (!newUser) {
+    json(
+      {
+        error: 'Something went wrong tryibg to create a new user',
+        fields: {
+          email: form.email,
+          username: form.username,
+          password: form.password
+        }
+      },
+      { status: 400 }
+    );
+  }
+
+  return null;
+};
+
+// login method
+export const login = async (form: LoginForm) => {
+  const user = await prisma.user.findFirst({
+    where: { OR: [{ email: form.username }, { username: form.username }] }
+  });
+
+  if (!user || !(await bcrypt.compare(form.password, user.password))) {
+    return json(
+      {
+        error: 'incorrect login'
+      },
+      { status: 400 }
+    );
+  }
+
+  return null;
 };
--- app/utils/types.server.ts
+++ app/utils/types.server.ts
@@ -3,3 +3,7 @@ export interface RegisterForm {
   username: string;
   password: string;
 }
+export interface LoginForm {
+  username: string;
+  password: string;
+}
